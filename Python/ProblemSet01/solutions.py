563f2c7ab71885dc2169f1842a43f8c3ec53b3b7
Hariniraghavan12 <33822772+Hariniraghavan12@users.noreply.github.com>
2019-03-05 10:21:03 +0530

    Update and rename solutions to solutions.py

--- Python/ProblemSet01/solutions.py
+++ Python/ProblemSet01/solutions.py
@@ -1,5 +1,5 @@
-1.Write a program that examines three variables—x, y, and z— and prints the largest 
-odd number among them. If none of them are odd, it should print a message to that effect.
+#1.Write a program that examines three variables—x, y, and z— and prints the largest 
+#odd number among them. If none of them are odd, it should print a message to that effect.
 
 list1=[]
 oddlist=[]
@@ -18,18 +18,18 @@ else:
     
     
     
-2.Python provides a built-in function called len that returns the length of a string,
-so the value of len('Cigna') is 5. Write a function named right_justify that takes a string named s as a parameter and prints
-the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
+#2.Python provides a built-in function called len that returns the length of a string,
+#so the value of len('Cigna') is 5. Write a function named right_justify that takes a string named s as a parameter and prints
+#the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
 
 def right_justify(s):
-     print(s.rjust(70))
+    print(s.rjust(70))
 right_justify("Cigna")
 
 
 
-3.Write a program that asks the user to input 10 integers, and then prints the 
-largest odd number that was entered. If no odd number was entered, it should print a message to that effect.
+#3.Write a program that asks the user to input 10 integers, and then prints the 
+#largest odd number that was entered. If no odd number was entered, it should print a message to that effect.
 
 list1=[]
 oddlist=[]
@@ -47,10 +47,10 @@ else:
     print("there are no odd numbers in the list")
     
     
-4.Practice using the Python interpreter as a calculator:
+#4.Practice using the Python interpreter as a calculator:
 
-a) The volume of a sphere with radius r is 4/3pr3. What is the volume of a sphere with radius 5?
-Hint: 392.7 is wrong!
+#a) The volume of a sphere with radius r is 4/3pr3. What is the volume of a sphere with radius 5?
+#Hint: 392.7 is wrong!
 
 import math
 radius=int(input("enter the radius:"))
@@ -58,9 +58,9 @@ volume=(4.0/3.0)*(math.pi)*(radius**3)
 print(volume)
 
 
-b) Suppose the cover price of a book is Rs.24.95, but bookstores get a 40% discount. Shipping costs
-Rs.3 for the first copy and 0.75p for each additional copy. What is the total wholesale cost for
-60 copies?
+#b) Suppose the cover price of a book is Rs.24.95, but bookstores get a 40% discount. Shipping costs
+#Rs.3 for the first copy and 0.75p for each additional copy. What is the total wholesale cost for
+#60 copies?
 
 cp=24.95
 copy1_shipping=3
@@ -73,9 +73,9 @@ total_copies=copies+copy1
 print(total_copies)
 
 
-5.Write a program that asks the user to enter an integer and prints two integers, root and pwr, such that 0 < pwr < 6 and
-root**pwr is equal to the integer entered by the user.
-If no such pair of integers exists, it should print a message to that effect.
+#5.Write a program that asks the user to enter an integer and prints two integers, root and pwr, such that 0 < pwr < 6 and
+#root**pwr is equal to the integer entered by the user.
+#If no such pair of integers exists, it should print a message to that effect.
 
 num=int(input("enter a number:"))
 for i in range(1,6):
@@ -85,8 +85,8 @@ for i in range(1,6):
         print("root is:",root)
         print("pwr is:",pwr)
         
-6.Let s be a string that contains a sequence of decimal numbers separated by commas, e.g., s = '1.23,2.4,3.123'. 
-Write a program that prints the sum of the numbers in s.
+#6.Let s be a string that contains a sequence of decimal numbers separated by commas, e.g., s = '1.23,2.4,3.123'. 
+#Write a program that prints the sum of the numbers in s.
 
 list1=[]
 numlist=[]
@@ -100,8 +100,8 @@ for i in numlist:
 print(sum)
 
 
-7.Write a function isIn() that accepts two strings as arguments and returns True if either string occurs anywhere in the other, 
-and False otherwise. Hint: you might want to use the built-in str operation in.
+#7.Write a function isIn() that accepts two strings as arguments and returns True if either string occurs anywhere in the other, 
+#and False otherwise. Hint: you might want to use the built-in str operation in.
 
 def isIn(str1,str2):
     if(str1.find(str2)!=-1):
@@ -112,7 +112,7 @@ def isIn(str1,str2):
 isIn("harini","harish")
 
 
-8.Implement a function that satisfies the specification. Use a try-except block.
+#8.Implement a function that satisfies the specification. Use a try-except block.
 
 def getRatios(vect1, vect2):
 	"""Assumes: vect1 and vect2 are lists of equal length of numbers
@@ -135,8 +135,8 @@ finally:
     print("finallyy")
     
 
-9.Assume that we execute the following assignment statements: width = 17 height = 12.0 delimiter = '.' For each of the following expressions, 
-write the value of the expression and the type (of the value of the expression).
+#9.Assume that we execute the following assignment statements: width = 17 height = 12.0 delimiter = '.' For each of the following expressions, 
+#write the value of the expression and the type (of the value of the expression).
 
 width=17
 height=12.0
@@ -166,7 +166,7 @@ print 'numGuesses =', numGuesses
 print ans, 'is close to square root of', x
 
 
-a. What would the code above return if the statement x = 25 were replaced by x = -25?
+#a. What would the code above return if the statement x = 25 were replaced by x = -25?
 
 #x = 25
 x=-25#infinite loop
@@ -187,8 +187,8 @@ print 'numGuesses =', numGuesses
 print ans, 'is close to square root of', x
 
 
-b. What would have to be changed to make the code above for finding an approximation to the cube root of both negative and positive numbers? 
-(Hint: think about changing low to ensure that the answer lies within the region being searched.)
+#b. What would have to be changed to make the code above for finding an approximation to the cube root of both negative and positive numbers? 
+#(Hint: think about changing low to ensure that the answer lies within the region being searched.)
 
 x=8
 epsilon = 0.01
